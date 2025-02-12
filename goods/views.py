9bca3d721d4a52b1bc64d7f5e905ec738d1b7f60
Roman Klyukin <roma_kl31@mail.ru>
2025-02-12 11:44:06 +0300

    Added slug converter for categories

--- goods/views.py
+++ goods/views.py
@@ -1,13 +1,20 @@
+from unicodedata import category
 from django.shortcuts import render
 
+import goods
 from goods.models import Products
 
-def catalog(request):
+def catalog(request, category_slug):
+
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
 
     products = Products.objects.all()
 
     context = {
         'title': 'Pizzeria - Каталог',
-        'products': products,
+        'goods': goods,
     }
     return render(request, 'goods/catalog.html', context)
