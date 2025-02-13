562c3e48fbf3a2a0ae0a8e69d15c561cf502eae9
Roman Klyukin <roma_kl31@mail.ru>
2025-02-13 22:57:20 +0300

    The best option for full-text search has been implemented

--- goods/utils.py
+++ goods/utils.py
@@ -1,7 +1,12 @@
 import keyword
 from django.db.models import Q
 
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 from goods.models import Products
 
@@ -9,14 +14,34 @@ from goods.models import Products
 def q_search(query):
     if query.isdigit() and len(query) <= 3:
         return Products.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel=' <span style="background-color: orange">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel=' <span style="background-color: orange">',
+            stop_sel="</span>",
+        )
+    )
 
+    return result
 
     # keywords = [word for word in query.split() if len(word) > 2]
 
